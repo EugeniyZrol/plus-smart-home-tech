16acef7934d8f16adb1c1ddef7e0292de8e25f77
EugeniyZrol <eu.zrol@yandex.ru>
2025-12-03 20:10:25 +0500

    инициализация БД

--- 01-init-schemas.sql
+++
@@ -1,5 +0,0 @@
-CREATE SCHEMA IF NOT EXISTS telemetry;
-
-GRANT ALL ON SCHEMA telemetry TO postgres;
-
-SET search_path TO telemetry;
--- 03-init-commerce-schemas.sql
+++
@@ -1,9 +0,0 @@
--- создаем схемы
-CREATE SCHEMA IF NOT EXISTS shopping_store;
-CREATE SCHEMA IF NOT EXISTS shopping_cart;
-CREATE SCHEMA IF NOT EXISTS warehouse;
-
--- Права
-GRANT ALL ON SCHEMA shopping_store TO postgres;
-GRANT ALL ON SCHEMA shopping_cart TO postgres;
-GRANT ALL ON SCHEMA warehouse TO postgres;
